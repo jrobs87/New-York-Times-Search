4059dad67ab69eff0f14b9e4ec0a6a32523b2464
John Robertson <tiktaalikdesign@gmail.com>
2019-03-30 14:07:05 -0400

    button hooks set up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,14 @@
-// [========== STAYED AT THE ACE HOTEL ==========]
-document.getElementById('hello').innerText = 'Stayed at the Ace Hotel! - JS';
-
-// A $( document ).ready() block.
 $(document).ready(function () {
     console.log("Stayed at the ace hotel!");
+
+    $('#search').on('click', function() {
+
+    });
+
+    $('#clearResults').on('click', function() {
+        event.preventDefault();
+        $('#results').empty();
+        console.log('Results have been cleared.');
+    });
+
 });
