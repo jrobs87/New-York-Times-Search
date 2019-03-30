cebd78372572f7810b21dd79ea59791d92aa4e04
John Robertson <tiktaalikdesign@gmail.com>
2019-03-30 13:27:19 -0400

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// [========== STAYED AT THE ACE HOTEL ==========]
+document.getElementById('hello').innerText = 'Stayed at the Ace Hotel! - JS';
+
+// A $( document ).ready() block.
+$(document).ready(function () {
+    console.log("Stayed at the ace hotel!");
+});
